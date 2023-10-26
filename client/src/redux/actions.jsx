fb1491dfb44e18c78096df4d99a885537ae8e85e
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-10-26 15:11:01 -0300

    admin

--- client/src/redux/actions.jsx
+++ client/src/redux/actions.jsx
@@ -68,7 +68,7 @@ export function createAdmin(payload) {
 		console.log('creando admin');
 		try {
 			const info = await axios.post(
-				'https://menu-final-production.up.railway.app/admin-boss',
+				'/admin-boss',
 				payload
 			);
 
